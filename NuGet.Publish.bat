1b6bfa2cc1228e68922f81f1caf1b8769050da4e
tk <torben.koenke@gmail.com>
2018-07-29 14:43:45 +0200

    Update Readme, NuGet...

--- NuGet.Publish.bat
+++ NuGet.Publish.bat
@@ -1,4 +1,6 @@
 @echo off
+set /p last=<last_version
+@echo Last published version is %last%
 set /p version="Version (ex. 4.2.0): "
 nuget.exe pack ICSharpCode.TextEditor.nuspec -Version %version%
 nuget.exe push ICSharpCode.TextEditor.Extended.%version%.nupkg
